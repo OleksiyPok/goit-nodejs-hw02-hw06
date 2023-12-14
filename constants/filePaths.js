479d6eba239fb63b6bc34d2742ece423657553e3
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-12-14 10:05:43 +0100

    db contacts has been moved

--- constants/filePaths.js
+++ constants/filePaths.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-const CONTACTS_FILE_PATH = path.join(__dirname, "../models/contacts.json");
+const CONTACTS_FILE_PATH = path.join(__dirname, "../db/contacts.json");
 
 const filePaths = {
   CONTACTS_FILE_PATH,
